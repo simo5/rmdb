4fe772434e03fa8867907be5cbac46230b31afa0
Simo Sorce <simo@samba.org>
2021-01-01 13:51:39 -0500

    Move tests to cargo test
    Signed-off-by: Simo Sorce <simo@samba.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1854,3 +1854,218 @@ impl Drop for RmdbWTxn<'_> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::thread;
+use std::time;
+
+use openssl::rand::rand_bytes;
+
+use super::*;
+
+fn db_filename(sname: &str) -> String {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        return args[1].clone();
+    }
+    String::from(sname)
+}
+
+fn get_rand_val(fill: u8, maxsize: usize) -> Vec<u8> {
+    let mut r = [0; 2];
+    rand_bytes(&mut r).unwrap();
+    let size = u16::from_le_bytes(r) as usize % maxsize;
+    vec![fill; size]
+}
+
+fn test(name: String, opt: Option<RmdbOptions>) {
+
+    let rmdb = Arc::new(Rmdb::create(PathBuf::from(name.clone()), opt).unwrap());
+    let mut txn = rmdb.get_write_transaction().unwrap();
+
+    let result = txn.add_entry(b"test", &get_rand_val(b'v', 65536));
+    match result {
+        Ok(()) => (),
+        Err(error) => {
+            println!("Add error: {}", error);
+            return();
+        },
+    };
+    let value = txn.get_entry(b"test").unwrap();
+    println!("Value = {}", std::str::from_utf8(&value[0]).unwrap());
+    txn.commit().unwrap();
+    drop(txn);
+    drop(rmdb);
+
+    /* reopen */
+    let rmdb = Arc::new(Rmdb::open(PathBuf::from(name)).unwrap());
+
+    let mut handles = vec![];
+    for t in 0..10 {
+        let rmdb = Arc::clone(&rmdb);
+        let handle = thread::spawn(move || {
+            if t % 2 == 0 {
+                let mut txn = rmdb.get_write_transaction().unwrap();
+                let key = format!("test{}", t);
+                let value = format!("value{}", t);
+                txn.add_entry(key.as_bytes(), value.as_bytes()).unwrap();
+                println!("Written '{}' from thread {}", key, t);
+                txn.commit().unwrap();
+            } else {
+                thread::sleep(time::Duration::from_secs(1));
+                let txn = rmdb.get_read_transaction().unwrap();
+                let key = format!("test{}", 9 - t);
+                match txn.get_entry(key.as_bytes()) {
+                    Ok(value) => {
+                        println!("Value '{}' from thread {}",
+                            std::str::from_utf8(&value[0]).unwrap(), t);
+                    },
+                    Err(error) => {
+                        println!("Error '{}' from thread {}", error, t);
+                    }
+                };
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let mut txn = rmdb.get_read_transaction().unwrap();
+    for i in [0,2,4,6,8].iter() {
+        let key = format!("test{}", i);
+        let value = txn.get_entry(key.as_bytes()).unwrap();
+        println!("Key/Value = {}/{}", key,
+            std::str::from_utf8(&value[0]).unwrap());
+    }
+
+    /* test cursor setting */
+    let key1 = "test4".as_bytes();
+    let key2 = "test6".as_bytes();
+    let mut cursor = txn.get_cursor().unwrap();
+    cursor.set_to(key1).unwrap();
+    let res = cursor.get_current().unwrap();
+    if res.0 != key1 {
+        println!("Setting cursor, expected {}, got {}",
+                 std::str::from_utf8(key1).unwrap(),
+                 std::str::from_utf8(res.0).unwrap());
+    }
+    let res = cursor.get_next().unwrap();
+    if res.0 != key2 {
+        println!("Setting cursor, expected {}, got {}",
+                 std::str::from_utf8(key2).unwrap(),
+                 std::str::from_utf8(res.0).unwrap());
+    }
+}
+
+fn testload(name: String, opt: Option<RmdbOptions>,
+            num: usize, vsize: usize, delete: bool) {
+
+    let rmdb = Rmdb::create(PathBuf::from(name.clone()), opt).unwrap();
+    let mut txn = rmdb.get_write_transaction().unwrap();
+
+    println!("Adding {} test records", num);
+
+    for i in 0..num {
+        let key = format!("test{}", i);
+        let value = get_rand_val(b'v', vsize);
+        let result = txn.add_entry(key.as_bytes(), &value);
+        match result {
+            Ok(()) => (),
+            Err(error) => {
+                println!("Adding Key {} with value length {}, got {}",
+                         key, value.len(), error);
+            },
+        };
+    }
+    txn.commit().unwrap();
+    drop(txn);
+
+    let mut txn = rmdb.get_read_transaction().unwrap();
+    for i in 0..num {
+        let key = format!("test{}", i);
+        let result = txn.get_entry(key.as_bytes());
+        match result {
+            Ok(_val) => (),
+            Err(error) => {
+                println!("Reading Key {} got {}", key, error);
+            },
+        };
+    }
+    /* also check we find all elements via cursor */
+    let mut keys = Vec::new();
+    for i in 0..num {
+        keys.push(format!("test{}", i));
+    }
+    let mut cursor = txn.get_cursor().unwrap();
+    while let Ok(x) = cursor.get_next() {
+        keys.retain(|k| k != std::str::from_utf8(x.0).unwrap());
+    }
+    if keys.len() != 0 {
+        println!("Could not find entries: {:?}", keys);
+    }
+    drop(txn);
+
+    if delete {
+        let mut txn = rmdb.get_write_transaction().unwrap();
+        for i in 0..num {
+            let key = format!("test{}", i);
+            let result = txn.del_entry(key.as_bytes());
+            match result {
+                Ok(_val) => (),
+                Err(error) => {
+                    println!("Deleting Key {} got {}", key, error);
+                },
+            };
+        }
+    }
+}
+
+#[test]
+fn main_test() {
+
+    let name = db_filename("test1db.rmdb");
+    /* remove file if exist, ignore errors */
+    let _ = fs::remove_file(name.as_str());
+
+    /* use default flags */
+    test(name, None);
+
+    let name = db_filename("test2db.rmdb");
+    /* remove file if exist, ignore errors */
+    let _ = fs::remove_file(name.as_str());
+
+    /* use no integrity and no flush, also small pagesize and db size */
+    let opt =  Some(*RmdbOptions::new()
+                                  .pagesize(1024)
+                                  .initial_size(1024 * 100)
+                                  .flags(RmdbFlags::empty()));
+    test(name, opt);
+
+    let name = db_filename("test3db.rmdb");
+    /* remove file if exist, ignore errors */
+    let _ = fs::remove_file(name.as_str());
+
+    /* smallest page size then fill db with more leaves than a single
+     * node can hold */
+    let opt =  Some(*RmdbOptions::new()
+                                  .pagesize(256)
+                                  .initial_size(1024 * 4096));
+    testload(name, opt, 64, 1024, false);
+
+    let name = db_filename("test4db.rmdb");
+    /* remove file if exist, ignore errors */
+    let _ = fs::remove_file(name.as_str());
+
+    /* More, entries, delete entries, enough entries to cause db growth */
+    let opt =  Some(*RmdbOptions::new().pagesize(128).initial_size(128*64));
+    testload(name, opt, 1024, 512, true);
+}
+}
