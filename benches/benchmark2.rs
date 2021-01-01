91c218c90e321804ec59c65d2715cc19456d782c
Simo Sorce <simo@samba.org>
2021-01-01 15:52:17 -0500

    Add randomized adds
    Here only the order is randomized.
    
    Randomized Adds
    test: 50000 entries, value size: 128, page size: 128 (5 times)
        Perf: 6.0188551848 seconds/iter, 120,377 ns/entry, 8307.23 entries/sec
        DB size: 21,561,472 bytes (431 bytes/item, overhead 336.72%)
    test: 50000 entries, value size: 128, page size: 1024 (5 times)
        Perf: 2.5290791003999997 seconds/iter, 50,581 ns/entry, 19770.04 entries/sec
        DB size: 102,761,472 bytes (2,055 bytes/item, overhead 1605.47%)
    test: 50000 entries, value size: 128, page size: 4096 (5 times)
        Perf: 4.9128602394000005 seconds/iter, 98,257 ns/entry, 10177.37 entries/sec
        DB size: 342,364,160 bytes (6,847 bytes/item, overhead 5349.22%)
    test: 50000 entries, value size: 1024, page size: 1024 (5 times)
        Perf: 8.3674532634 seconds/iter, 167,349 ns/entry, 5975.53 entries/sec
        DB size: 154,534,912 bytes (3,090 bytes/item, overhead 301.76%)
    test: 50000 entries, value size: 1024, page size: 4096 (5 times)
        Perf: 5.029640730600001 seconds/iter, 100,592 ns/entry, 9941.07 entries/sec
        DB size: 342,364,160 bytes (6,847 bytes/item, overhead 668.65%)
    
    Signed-off-by: Simo Sorce <simo@samba.org>

--- benches/benchmark2.rs
+++ benches/benchmark2.rs
@@ -4,6 +4,9 @@ use std::time::SystemTime;
 
 use num_format::{SystemLocale, ToFormattedString};
 
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 use rmdb::Rmdb;
 use rmdb::RmdbFlags;
 use rmdb::RmdbOptions;
@@ -18,7 +21,7 @@ fn prepdata(keynum: usize, valsize: usize) -> (Vec<String>, Vec<u8>) {
     for _ in 0..val.capacity() {
         val.push("v".as_bytes()[0]);
     }
-    (keys,val)
+    (keys, val)
 }
 
 fn opendb(keynum: usize, pagesize: usize) -> Rmdb {
@@ -44,8 +47,24 @@ fn iteradd(rmdb: &Rmdb, keys: &Vec<String>, value: &Vec<u8>) -> f64 {
     start.elapsed().unwrap().as_secs_f64()
 }
 
-fn bench_add_seq() {
+fn summary(entries: f64, iters: f64, sum: f64, vsize: f64, dbsize: u64) {
     let locale = SystemLocale::default().unwrap();
+
+    let iter = sum / iters;
+    let item = (iter / entries * 1000000000f64) as u64;
+    let items = entries / iter;
+    println!("    Perf: {} seconds/iter, {} ns/entry, {:.2} entries/sec",
+             iter, item.to_formatted_string(&locale), items);
+    let item = dbsize / entries as u64;
+    println!("    DB size: {} bytes ({} bytes/item, overhead {:.2}%)",
+             dbsize.to_formatted_string(&locale),
+             item.to_formatted_string(&locale),
+             item as f64/ vsize * 100f64);
+}
+
+fn bench_add_seq() {
+
+    println!("Sequential Adds");
 
     for b in &[(128, 128),
                (128, 1024),
@@ -72,19 +91,45 @@ fn bench_add_seq() {
             size = rmdb.size().unwrap();
         }
 
-        let iter = sum / iterations as f64;
-        let item = (iter / entries as f64 * 1000000000f64) as u64;
-        let items = entries as f64 / iter;
-        println!("    Perf: {} seconds/iter, {} ns/entry, {:.2} entries/sec",
-                 iter, item.to_formatted_string(&locale), items);
-        let item = size / entries as u64;
-        println!("    DB size: {} bytes ({} bytes/item, overhead {:.2}%)",
-                 size.to_formatted_string(&locale),
-                 item.to_formatted_string(&locale),
-                 item as f64/ b.0 as f64 * 100f64);
+        summary(entries as f64, iterations as f64, sum, b.0 as f64, size);
+    }
+}
+
+fn bench_add_rnd() {
+
+    println!("Randomized Adds");
+
+    for b in &[(128, 128),
+               (128, 1024),
+               (128, 4096),
+               (1024, 1024),
+               (1024, 4096)] {
+
+        let iterations = 5;
+        let entries = 50000;
+        println!("test: {} entries, value size: {}, page size: {} ({} times)",
+                 entries, b.0, b.1, iterations);
+
+        let mut sum = 0f64;
+        let mut size = 0u64;
+
+        let (mut keys, value) = prepdata(entries, b.0);
+        keys.shuffle(&mut thread_rng());
+
+        for _ in 0..iterations {
+            let rmdb = opendb(entries, b.1);
+
+            let elapsed = iteradd(&rmdb, &keys, &value);
+
+            sum = sum + elapsed;
+            size = rmdb.size().unwrap();
+        }
+
+        summary(entries as f64, iterations as f64, sum, b.0 as f64, size);
     }
 }
 
 fn main() {
     bench_add_seq();
+    bench_add_rnd();
 }
